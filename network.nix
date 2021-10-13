f879d56094925040c5d9288504ad0f30ed884507
Zach Latta <zach@zachlatta.com>
2021-10-13 17:39:09 -0400

    Add secrets file configuration

--- network.nix
+++ network.nix
@@ -2,8 +2,17 @@
   network.description = "proxy deployment on Linode";
 
   "proxy" = { config, pkgs, lib, ... }: {
-    deployment.targetUser = "root";
-    deployment.targetHost = "proxy.servers.hackclub.com";
+    deployment = {
+      targetUser = "root";
+      targetHost = "proxy.servers.hackclub.com";
+
+      secrets = {
+        "acme-dns-credentials" = {
+          source = "./secrets/acme_dns_credentials.env";
+          destination = "/var/secrets/acme_dns_credentials.env";
+        };
+      };
+    };
 
     imports = [ ./proxy/configuration.nix ];
   };
