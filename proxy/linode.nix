8449a505359a17d1f36162ad35dda1816e6b1399
Zach Latta <zach@zachlatta.com>
2021-10-13 17:00:33 -0400

    Get IPv6 working on Linode

--- proxy/linode.nix
+++ proxy/linode.nix
@@ -3,6 +3,8 @@
 { pkgs, ... }: {
   networking.useDHCP = false;
   networking.interfaces.eth0.useDHCP = true;
+  networking.interfaces.eth0.tempAddress =
+    "disabled"; # needed for ipv6 to work on Linode, https://github.com/NixOS/nixpkgs/issues/71273#issuecomment-545442741
 
   networking.usePredictableInterfaceNames = false; # needed for linode
 
