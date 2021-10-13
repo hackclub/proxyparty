d01e2e81a4f38fbc4926fed641b8b5a3bc384909
Zach Latta <zach@zachlatta.com>
2021-10-13 12:35:41 -0400

    Get very basic nginx config generation working

--- proxy/nginx/default.nix
+++ proxy/nginx/default.nix
@@ -1,5 +1,49 @@
-{ config, pkgs, ... }: {
+{ config, pkgs, lib, ... }:
+let
+  rules = import ../../rules.nix;
+
+  # from https://stackoverflow.com/a/54505212
+  recursiveMerge = with lib;
+    attrList:
+    let
+      f = attrPath:
+        zipAttrsWith (n: values:
+          if tail values == [ ] then
+            head values
+          else if all isList values then
+            unique (concatLists values)
+          else if all isAttrs values then
+            f (attrPath ++ [ n ]) values
+          else
+            last values);
+    in f [ ] attrList;
+
+  rulesConvertedToNginxConfig = recursiveMerge (map (rule:
+    if builtins.hasAttr "redirect" rule then {
+      virtualHosts."${rule.redirect}" = {
+        locations."/".return = "302 ${rule.dest}/$request_uri";
+      };
+    } else if builtins.hasAttr "proxy" rule then {
+      virtualHosts."${rule.proxy}" = {
+        locations."/" = {
+          proxyPass = "${rule.dest}/$request_uri";
+          proxyWebsockets = true;
+          extraConfig = ''
+            # required when the destination is also a TLS server with multiple hosts
+            proxy_ssl_server_name on;
+          '';
+        };
+      };
+    } else
+      abort "Can't find 'proxy', 'redirect', or 'permRedirect' in ${
+        builtins.toJSON rule
+      }") rules);
+in {
   networking.firewall.allowedTCPPorts = [ 80 ];
 
-  services.nginx = { enable = true; };
+  services.nginx = {
+    enable = builtins.trace (builtins.toJSON rulesConvertedToNginxConfig) true;
+
+    resolver = { addresses = [ "1.1.1.1" ]; };
+  } // rulesConvertedToNginxConfig;
 }
