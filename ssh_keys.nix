17da54cda9b7495c92b204f45c487d875536dc5d
Zach Latta <zach@zachlatta.com>
2021-10-13 12:35:28 -0400

    Add abra SSH key

--- ssh_keys.nix
+++ ssh_keys.nix
@@ -2,6 +2,7 @@
   zrl = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEwQfzs4sQYgvcVKlWFbBKPmFH1yPFBGi4lJMrIlYm/5 zrl@lugia"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICSLDfNoAhyP89I99ZgepL6LiZE2jK6A4cqGR4CNceUb zrl@psyduck"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIDO1VSoHnkHOK3ZCqqCRCxtzVtEFast1EWKgFne3tnN zrl@abra"
   ];
 
   msw = [
