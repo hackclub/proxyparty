da5ec1574bb027a06d8b402052cb555e755ff095
Zach Latta <zach@zachlatta.com>
2021-10-14 11:41:53 -0400

    Set up GitHub Action for deployment

--- ssh_keys.nix
+++ ssh_keys.nix
@@ -8,4 +8,8 @@
   msw = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII5bJhhyjxGBe/3cU7NzbLBuX6hFn+nkgx3HD/nNDf4B max@rubber"
   ];
+
+  github-actions = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL4/L67pKMn7tk24rjTUOSVW7Zk70+bXS7zlaV956pSU proxy@github-actions"
+  ];
 }
