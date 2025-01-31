9446b90f7dfd2d5c73a2ee13a1f93811f810ff38
Cheru Berhanu <mail@cheru.dev>
2025-01-31 13:04:10 -0500

    Update hardware-configuration.nix

--- proxy/hardware-configuration.nix
+++ proxy/hardware-configuration.nix
@@ -1,6 +1,8 @@
 { modulesPath, ... }: {
   imports = [ (modulesPath + "/profiles/qemu-guest.nix") ];
 
+  nixpkgs.hostPlatform = "x86_64-linux";
+
   boot.initrd.kernelModules = [ "nvme" ];
 
   fileSystems."/" = {
